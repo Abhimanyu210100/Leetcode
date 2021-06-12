4ae419c71e07918174925fb1b82a8841cf6c74f9
Abhimanyu <abhiswaroop210100@gmail.com>
2021-06-12 23:36:17 +0530

    Practice

---
+++ Linked List/MergeTwoSorted.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        tail = dummy
+        while True:
+            if l1 == None:
+                tail.next = l2
+                break
+            if l2 == None:
+                tail.next = l1
+                break
+            if l1.val >= l2.val:
+                tail.next = l2
+                l2 = l2.next
+            else:
+                tail.next = l1
+                l1 = l1.next
+            tail = tail.next
+        return dummy.next
+        
